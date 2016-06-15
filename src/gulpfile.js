b2e334431fc6faf4f15f059397b6d2d3b5dae9e9
Mia Wang <mia.d.wang@gmail.com>
2016-06-16 02:29:37 +0800

    Set production from development. Minify css and js
    Modify images in img folder
    I should have made two folders earlier

---
+++ src/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    htmlmin = require('gulp-htmlmin'),
+    rename = require('gulp-rename'),
+    minifyInline = require('gulp-minify-inline'),
+    uglify = require('gulp-uglify'),
+    cleancss = require('gulp-clean-css');
+
+gulp.task('minify', function() {
+  gulp.src('./*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(minifyInline())
+    .pipe(gulp.dest('../dist/'));
+
+  gulp.src('./css/*.css')
+    .pipe(cleancss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('../dist/css/'));
+
+  gulp.src('./js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('../dist/js/'));
+});
+
+gulp.task('default', ['minify']);
